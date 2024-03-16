517d0eb82f274992c85ab2e7af6870756a35cbdc
Mateus Merçon <mateusmercon@gmail.com>
2024-03-16 20:23:55 -0300

    Initial commit

---
+++ src/main/java/com/api/pagamentofuncionarios/PagamentoFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.api.pagamentofuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoFuncionariosApplication.class, args);
+	}
+
+}
